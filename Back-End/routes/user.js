6e78c61e4de3a27ab64802a83f0234e75be4d88b
satish <satishkonduru0225@gmail.com>
2023-12-04 18:00:26 +0530

    nodemailer

--- Back-End/routes/user.js
+++ Back-End/routes/user.js
@@ -1,6 +1,18 @@
 const express = require('express')
 const connection =  require('../connection')
 const router = express.Router()
+require('dotenv').config()
+const nodemailer = require('nodemailer')
+
+
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD
+    }
+})
+
 
 router.post('/signup', (req, res)=>{
     let user = req.body
@@ -24,6 +36,40 @@ router.post('/signup', (req, res)=>{
         }
         else{
           return res.status(500).json(err)  
+        }
+    })
+})
+
+
+router.post('/forgotPassword', (req, res) => {
+    const user = req.body
+    query = "select email, password from user where email=?"
+    connection.query(query, [user.email], (err, results) => {
+        if(!err){
+            if(results.length <= 0){
+                return res.status(200).json({message: 'Password sent.'})
+            }
+            else{
+                var mailOptions = {
+                    from: process.env.EMAIL,
+                    to: results[0].email,
+                    subject: 'Password sent by RSKMart',
+                    html: '<p><b>Password:</b> <h1>'+results[0].password+'</h1></p>'
+                }
+                transporter.sendMail(mailOptions, (err, info) => {
+                    if(err){
+                        console.log(err)
+                    }
+                    else{
+                        console.log("Email sent")
+                        return res.status(200).json({message: 'Password Sent to your gmail.'})
+                    }
+                })
+            }
+
+        }
+        else{
+            return res.status(500).json(err)
         }
     })
 })
